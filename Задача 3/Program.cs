b123f84a43d6bf7975ac4846f706955d89aab4c7
Irina <irina-obik@yandex.ru>
2022-05-12 01:03:50 +0500

    Задача 3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая задаёт массив из некоторого количества элементов 
+// и выводит их на экран с помощью функций. (можно спрашивать количество вводимых 
+// элементов и вводить все значения с клавиатуры)
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите числа через запятую: ");
+string _numbers = Console.ReadLine();
+string[] _arrayNumbers = _numbers.Split(new char[] {','});
+
+for(int i = 0; i < _arrayNumbers.Length; i++)
+{
+    Console.Write($"{_arrayNumbers[i]} ");
+}
+Console.WriteLine();
